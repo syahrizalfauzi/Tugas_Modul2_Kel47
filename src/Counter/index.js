d8380217c5e5b2e6c468d36221bf10a45a80e993
M Dzaky Naufal <muhammaddzakynaufaln@gmail.com>
2021-09-11 20:27:52 +0800

    Update index.js

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-function Button() {
-  const [count, setCount] = React.useState(0);
+function Button(props) {
   const { blue, italic, isClicked } = props;
+  const [count, setCount] = React.useState(0);
   return (
     <div>
       <p>Jumlahnya {count} buah</p>
       <button onClick={() => setCount(count + 1)}>
-        Tambahkan
-        <input type="submit" clicked={isClicked}></input>
-        {count.useState((isClicked) => (
-          <b
-            className={`${blue ? "blue" : ""} ${italic ? "italic" : ""} ${
-              isClicked ? "red" : ""
-            }`}
-          ></b>
-        ))}
+        <b
+          className={`${blue ? "blue" : ""} ${italic ? "italic" : ""} ${
+            isClicked ? "red" : ""
+          }`}
+        >
+          Tambahkan
+          <input type="submit" clicked={isClicked}></input>
+        </b>
       </button>
     </div>
   );
