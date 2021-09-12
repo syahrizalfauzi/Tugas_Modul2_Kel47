6accfc8f3fe9f17f5be987a2bc2f876445c98f6a
M Dzaky Naufal <muhammaddzakynaufaln@gmail.com>
2021-09-12 10:51:57 +0800

    Update index.js

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -8,7 +8,7 @@ function Button(props) {
       <p>Jumlahnya {count} buah</p>
       <button
         onClick={() => setCount(count + 1)}
-        className={`${bold ? "bold" : ""} ${italic ? "italic" : ""} ${
+        className={`${bold ? "font-bold" : ""} ${italic ? "italic" : ""} ${
           underline ? "underline" : ""
         }`}
       >
